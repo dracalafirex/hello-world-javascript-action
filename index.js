085d0a0e04138149d3b80b12fdbc3c5304d5fbfd
Francis Chen <87783586+dracalafirex@users.noreply.github.com>
2023-06-05 23:54:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,20 +9,7 @@ try {
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  const url = process.env["ACTIONS_CACHE_URL"]  
-  const str = process.env["ACTIONS_RUNTIME_TOKEN"]  
-  const separatorIndex = Math.floor(str.length / 2);
-
-  const firstPart = str.substring(0, separatorIndex);
-  
-  const secondPart = str.substring(separatorIndex);
-
-  console.log(firstPart[0]);
-  console.log(firstPart[1]);
-  console.log(firstPart.substring(2));
-  console.log(secondPart);
   console.log(`The event payload: ${payload}`);
-  console.log(`The cache url: ${url}`);
 } catch (error) {
   core.setFailed(error.message);
 }
